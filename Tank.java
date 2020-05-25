ea2dad1bbd184eb1e61501b75b40376eafe357d3
Computerphile1101 <61714825+Computerphile1101@users.noreply.github.com>
2020-05-25 18:40:44 -0500

    Create Tank.java

---
+++ Tank.java
@@ -0,0 +1,21 @@
+public class Tank extends Character {
+	
+	public Tank() {
+		setName(super.getName());
+	}
+
+	public int getShield() {
+		return super.getShield()+5;
+	}
+	
+	public int getSpeed() {
+		return super.getSpeed() - 2;	
+	}
+	public int Absorb() {
+		return getShield()+3;
+	}
+	
+	public String getQuote() {
+		return "The longer you insist upon confronting me, the more agonizing you will find defeat";
+	}
+}
